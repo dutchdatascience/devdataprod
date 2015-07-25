eb7459669f35c343445498380a281a78bbc26cd7
Dutchdatascience <dutchdatascience@users.noreply.github.com>
2015-07-25 12:18:02 -0400

    added description

--- ui.R
+++ ui.R
@@ -9,9 +9,10 @@ fluidPage(
     sliderInput('sd', 'Standard deviation', min=0.3, 10
                , value=1, step=0.1),
 
-    selectInput('color', 'Color', colors)
+    selectInput('color', 'Color', colors),
+    h5('Use the slider to choose another standard deviation.'),
+    h5('Use the dropdown list to choose another color.')
   ),
-  
   mainPanel(
     plotOutput('plot')
   )
